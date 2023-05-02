d43748156fd0765cac108c292fba7c4296ed9c4e
Thomas Ramiro <116609321+ThomRamks@users.noreply.github.com>
2023-05-02 19:43:51 -0300

    Finalizando.

--- src/main/java/br/com/ada/testeautomatizado/exception/ListaVaziaException.java
+++
@@ -1,7 +0,0 @@
-package br.com.ada.testeautomatizado.exception;
-
-public class ListaVaziaException extends RuntimeException {
-    public ListaVaziaException() {
-        super("Nao ha veiculos a serem listados");
-    }
-}
